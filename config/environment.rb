8c6cb3c4b25ed3b9ff0ac99c9472e13014570a19
Allan <kipropsallan@gmail.com>
2022-08-30 14:10:09 +0300

    commit tree 54f5ed0219d14d8704d47826856e1e8340b91089

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,4 @@
-# Set up Bundler here following the instructions in the README
+# Set up Bundler here following the instructions in the READMEs
+
+require 'bundler/setup'
+Bundler.require(:default, :development)
